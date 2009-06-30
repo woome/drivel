93f0472cb9d3e63af0d13960e2aa590f1af08853
Edward George <edwardgeorge@gmail.com>
2009-06-30 14:50:28 +0100

    added caching of user data

--- drivel/contrib/django.py
+++ drivel/contrib/django.py
@@ -10,8 +10,14 @@ def MemcacheAuthBackend(server):
         sessionid = request.cookies.get(session_cookie)
         session = server.send('memcache', 'get', sessionid).wait()
         userid = session.get('_auth_user_id')
+        cachekey = 'user_object_for_id_%s' % userid
+        cachehit = server.send('memcache', 'get', cachekey).wait()
+        if cachehit:
+            return cachehit
         rows = server.send('db', 'SELECT username, password FROM auth_user '
             'WHERE id = %s', [userid]).wait()
-        return User(userid, *rows[0])
+        user = User(userid, *rows[0])
+        server.send('memcache', 'set', cachekey, user)
+        return user
     return doauth
 
